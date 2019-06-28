ba8e91c3735bb439ba4ebd1e142741ca9b3d3bfd
Vannear <30491653+vannearpor@users.noreply.github.com>
2019-06-27 22:30:31 -0700

    Player move functions

--- player.py
+++ player.py
@@ -66,6 +66,18 @@ class Player:
 
     def view_inventory(self):
         #prints item in the inventory list
+        print()
+        print("Here are the items you are currently holding in your inventory:")
+        print()
+        
+        if self.inventory:
+            for item in self.inventory:
+                print("{0}".format(item.name))
+                print()
+
+        else:
+            print("NONE. You currently have no items in your inventory.")
+            print()
 
     def fight(self, item, character):
         #check if in right room
@@ -78,16 +90,45 @@ class Player:
     def go_north(self):
         #grab players current location
         #go into room object and see what the north room is
+        if self.location.north_room is not None:
+            self.location = self.location.north_room
         #if room available
+
+        else:
+            print("There is nowhere to go north of {0}".format.self.location)
             #print description of going into new room, set player's location to new room
             #initiate player look, print description
             #print items seen in the room
 
     def go_south(self):
+        #grab players current location
+        #go into room object and see what the north room is
+        if self.location.south_room is not None:
+            self.location = self.location.south_room
+        #if room available
+        
+        else:
+            print("There is nowhere to go south of {0}".format.self.location)
 
     def go_west(self):
+        #grab players current location
+        #go into room object and see what the north room is
+        if self.location.west_room is not None:
+            self.location = self.location.west_room
+        #if room available
+        
+        else:
+            print("There is nowhere to go west of {0}".format.self.location)
 
     def go_east(self):
+        #grab players current location
+        #go into room object and see what the north room is
+        if self.location.east_room is not None:
+            self.location = self.location.east_room
+        #if room available
+        
+        else:
+            print("There is nowhere to go east of {0}".format.self.location)
 
     #place this function in game file - def help(self):
         #print out list of verbs that the user can use and some brief descriptions
